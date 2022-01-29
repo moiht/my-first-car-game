937a5ff0fdadd1cb4313966db7822079cef71629
moiht <71550278+moiht@users.noreply.github.com>
2022-01-29 10:58:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function preload(){
+backgroundimg = loadImage("assests/download.jpeg");
+
+}  
+
+
+
+function setup() {
+  createCanvas(1200,800);
+  
+}
+
+function draw() {
+  background(backgroundimg);  
+  drawSprites();
+}
